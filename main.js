eb2e0854ee4edeb04983cdf78575595b720b4f69
VeronicaPallas <veritopallas14@gmail.com>
2023-03-17 13:56:29 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const d = document;
+const input = d.querySelector("input");
+const addBtn = d.querySelector(".btn-add");
+const ul = d.querySelector("ul");
+const empty = d.querySelector(".empty");
+
+const addDeleteBtn = () => {
+  const deleteBtn = d.createElement("button");
+
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "btn-delete";
+
+  deleteBtn.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
+
+    const items = d.querySelectorAll("li");
+
+    if (items.length === 0) {
+      empty.style.display = "block";
+    }
+  });
+
+  return deleteBtn;
+};
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text !== "") {
+    const li = d.createElement("li");
+    const p = d.createElement("p");
+    p.textContent = text;
+
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    ul.appendChild(li);
+
+    input.value = "";
+    empty.style.display = "none";
+  }
+});
